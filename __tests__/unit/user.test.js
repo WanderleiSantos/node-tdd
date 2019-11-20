75a4977225761d3bd798c8a2cca7e34d66ed9024
Wanderlei Santos <wanderleidias.santos@hotmail.com>
2019-11-20 01:14:33 -0300

    Teste Node aplicando TDD com Jest

---
+++ __tests__/unit/user.test.js
@@ -0,0 +1,22 @@
+const bcrypt = require("bcryptjs");
+
+const { User } = require("../../src/app/models");
+const truncate = require("../utils/truncate");
+
+describe("User", () => {
+  beforeEach(async () => {
+    await truncate();
+  });
+
+  it("should encrypt user password", async () => {
+    const user = await User.create({
+      name: "Wanderlei",
+      email: "a@aa.com",
+      password: "123456"
+    });
+
+    const hash = await bcrypt.hash("123456", 8);
+
+    expect(await bcrypt.compare("123456", user.password_hash)).toBe(true);
+  });
+});
